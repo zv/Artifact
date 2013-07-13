43337e0b8e1d65198963d0c8140025f66320195e
Zephyr <zephyr.pellerin@gmail.com>
2013-07-13 13:53:11 -0700

    Checked in base Artifact application behaviour

--- lib/artifact.ex
+++ lib/artifact.ex
@@ -1,2 +1,27 @@
-defmodule ArtifactElixir do
+defmodule Artifact do
+  use Application.Behaviour
+  
+  def config([], acc) do
+    acc 
+  end
+
+  @doc """
+    Returns Artifact's current configuration 
+  """
+  def config([key | rest], acc) do
+    case System.get_env(:artifact, key) do 
+      {:ok, value } -> config(rest, [ {key, value} | acc ])
+      :undefined     -> config(rest, acc)
+    end
+  end
+
+  # Application behaviour callbacks.
+  def start(_type, _args) do
+    Application.start(:artifact)
+  end
+
+  def stop(_state) do
+    :ok
+  end
+
 end
