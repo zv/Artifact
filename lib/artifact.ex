d4f2484413ce5deb895f28d31d355b01e67cc4a2
Zephyr <zephyr.pellerin@gmail.com>
2013-07-16 14:05:05 -0700

    Checked in global artifact module documentation

--- lib/artifact.ex
+++ lib/artifact.ex
@@ -1,5 +1,18 @@
 defmodule Artifact do
-  use Application.Behaviour
+  @moduledoc """
+
+  Artifact is a performant, scalable distributed key-value store system,
+  whose intellectual ancestor is Amazon's Dynamo database model, but serves as
+  one of (if not the only) implementation of the SILT database concept.
+
+  It's open source and freely available on github. Unstructured and permissive
+  in what it accepts, strict in what it delivers -- perfect for storing large
+  numbers of small (<128k) data records which need to be read or processed in a
+  realtime, low-latency environment.
+     
+  This is the main module in the Artifact repository. 
+
+  """ 
   
   def config([], acc) do
     acc 
