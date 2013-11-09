178802f9d7373f41d3619af5b4aa290659f3c82f
Zephyr <zephyr.pellerin@gmail.com>
2013-11-09 12:42:08 -0800

    Modernized configuration to use mix.exs

--- lib/artifact.ex
+++ lib/artifact.ex
@@ -9,31 +9,28 @@ defmodule Artifact do
   in what it accepts, strict in what it delivers -- perfect for storing large
   numbers of small (<128k) data records which need to be read or processed in a
   realtime, low-latency environment.
-     
-  This is the main module in the Artifact repository. 
 
-  """ 
-  
+  This is the main module in the Artifact repository.
+
+  """
+
   use Application.Behaviour
 
+  @supported_options %w(rpc interfaces buckets vnodes tables store n r w)a
 
-  def config(keys, acc) when length(keys) == 0 do
-    acc 
+  def config([], acc) do
+    acc
   end
 
   def config([key | tail], acc) do
-    case :application.get_env(:artifact, key) do 
+    case :application.get_env(:artifact, key) do
       {:ok, value } -> config(tail, [ {key, value} | acc ])
       :undefined    -> config(tail, acc)
     end
   end
 
-  def start(_type, _args) do 
-    args = config([
-            :rpc, :interfaces, :buckets, :vnodes, :tables, :store,
-            :participants, :read_participants, :write_participants,
-            :hostname
-      ], [])
+  def start(_type, _args) do
+    args = config([ @supported_options ], [])
     :artifact_supervisor.start_link(args)
   end
 
