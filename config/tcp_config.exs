b295b7e2ea370c2bc6b61e8c92a050ed19932377
Zephyr Pellerin <zv@nxvr.org>
2016-12-10 20:26:57 -0800

    Consolidate Mix TCP configuration

--- config/tcp_config.exs
+++ config/tcp_config.exs
@@ -1,18 +1,1 @@
 use Mix.Config
-
-config :artifact, TCP,
-  listen: [
-    {:active, false},
-    :binary,
-    {:packet, :line},
-    {:reuseaddr, true}
-  ],
-  port: 11211,
-  max_processes: 8,
-  max_restarts: 3,
-  time: 60,
-  shutdown: 2000,
-  accept_timeout: :infinity,
-  accept_error_sleep_time: 3000,
-  recv_length: 0,
-  recv_timeout: :infinity
