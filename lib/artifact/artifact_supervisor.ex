b7a5099ab3973580a47462704c9f9a207003e98d
Zephyr Pellerin <zv@nxvr.org>
2016-12-11 20:35:57 -0800

    Switch supervision strategy to `one_for_all`

--- lib/artifact/artifact_supervisor.ex
+++ lib/artifact/artifact_supervisor.ex
@@ -1,21 +1,21 @@
 defmodule Artifact.Supervisor do
   import Supervisor.Spec
 
-  def start_link(_args), do: Supervisor.start_link(__MODULE__, :ok)
+  def start_link(), do: Supervisor.start_link(__MODULE__, :ok)
 
   def init(:ok) do
     tree = [
-      worker(Artifact.Logging, [[name: Logging]]),
-      worker(Artifact.Hash, [[name: Hash]]),
-      worker(Artifact.Store, [[name: Store]]),
-      worker(Artifact.State, [[name: Stat]]),
-      worker(Artifact.Connection, [[name: Connection]]),
-      worker(Artifact.Sync, [[name: Sync]]),
-      worker(Artifact.Membership, [[name: Membership]]),
-      worker(Artifact.RPC, [[name: RPC]]),
-      worker(Artifact.Memcache, [[name: Memcache]])
       worker(Artifact.Config, [Application.get_all_env(:artifact)]),
+      worker(Artifact.Logging, []),
+      worker(Artifact.Hash, []),
+      worker(Artifact.Store, []),
+      worker(Artifact.Stat, []),
+      worker(Artifact.Connection, []),
+      worker(Artifact.Sync, []),
+      worker(Artifact.Membership, []),
+      worker(Artifact.RPC, []),
+      worker(Artifact.Memcache, [[name: Memcache]])
     ]
-    supervise(tree, strategy: :one_for_one)
+    supervise(tree, strategy: :one_for_all)
   end
 end
